9eaa77326bfc65d766d663136582f48b289bdada
Geert <geertoff@gmail.com>
2024-05-31 09:54:42 +0200

    removed / added comments

--- sarpsborg-infographic/transformation_analysis.sql
+++ sarpsborg-infographic/transformation_analysis.sql
@@ -1,5 +1,12 @@
+/*
+	Input: 
+	- Agricultural field (field)
+	- building filtered by housing type, point and polygon geometry 
+	- information of amount of households that are dependent on the yield of a particular crop
+*/
+
 -- join information of bygning
-create table nibio.bygning as 
+create table bygning as 
 select 			b.objid,
 				b.objtype, 
 				b.naringsgruppe,
@@ -12,28 +19,24 @@ select 			b.objid,
 				b.kommunenavn,
 				b.bygningstype,
 				b.representasjonspunkt as geom,
-				st_transform( b.representasjonspunkt, 5973) as geom_5973 
 from matrikkelenbygning.bygning b 
 left join matrikkelenbygning.bygningsstatuskode st on b.bygningsstatus = st.identifier 
 left join matrikkelenbygning.bygningstypekode typ on b.bygningstype = typ.identifier 
 left join matrikkelenbygning.naringsgruppekode n on b.naringsgruppe = n.identifier;
 
 -- add primary key
-alter table nibio.bygning  add primary key (objid);
-
+alter table bygning  add primary key (objid);
 
 -- spatial indexes
-create index bygning_geom_idx on nibio.bygning using GIST (geom);
-create index bygning_geom_5973_idx on nibio.bygning using GIST (geom_5973);
-create index buildings_geom_idx on nibio.buildings_nibio using GIST (geom);
+create index bygning_geom_idx on bygning using GIST (geom);
+create index buildings_geom_idx on buildings_nibio using GIST (geom);
 
-
--- filter housing objects with a view
-create view nibio.v_bygning_bolig as 
+-- filter objects to housing objects
+create view v_bygning_bolig as 
 select * from bygning b where naringsgruppe = 'X';
 
 -- spatial joining the housing objects with the naringsgruppe. Multiple points can be in one polygon
-create view nibio.v_bolig_building as 
+create view v_bolig_building as 
 select 		row_number() over ()  objectid,
 			b.id,
 			vb.objid,
@@ -47,38 +50,31 @@ select 		row_number() over ()  objectid,
 			vb.kommunenummer,
 			vb.kommunenavn,
 			vb.bygningstype,
-		--	count(vb.geom_5973) as count_bygning,
-			vb.geom_5973 as centroid,
+			vb.geom as centroid,
 			b.geom
-from nibio.v_bygning_bolig vb
+from v_bygning_bolig vb
 join buildings_nibio b 
-on ST_CONTAINS(b.geom, vb.geom_5973);
+on ST_CONTAINS(b.geom, vb.geom);
 
--- generate spatial are
-select st_area(geom) from jordbruk j;
+-- generate spatial area of field
+select st_area(geom) from field f;
 
 -- nearest neighbour of points
-select * from v_bygning_bolig vbb; \
+select * from v_bygning_bolig vbb; 
 
-
--- centroid of jordbruk
-select st_distance(vb.geom_5973, st_centroid(j.geom)) as dist from jordbruk j, v_bygning_bolig vb
-order by dist asc; 
-
-/* use the centroid of the agricultural parcel and calculate the distance between a household and the parcel
- *  potato households: 6623
-	bread households; 2,293
- * 
- */
+/* 
+   use the centroid of the agricultural field and calculate the distance between a household and the field
+   the limit depends on the amount of households that can be 'fet'
+*/
 select 
-			vb.objid, 
-			st_distance(vb.geom_5973, st_centroid(j.geom)) as dist,
-			vb.geom_5973,
+			row_number() over ()  objectid, 
+			st_distance(vb.geom, st_centroid(f.geom)) as dist,
+			vb.geom,
 			bn.geom
-			
-from 		jordbruk j, v_bygning_bolig vb
-join 		buildings_nibio bn on ST_Contains(bn.geom, vb.geom_5973)			
-order by 	vb.geom_5973 <->  st_centroid(j.geom)
+from 		field f, v_bygning_bolig vb
+join 		buildings_nibio bn on ST_Contains(bn.geom, vb.geom)			
+order by 	vb.geom <->  st_centroid(f.geom)
+-- count of amount of households
 limit 6623;
 
 -- creating a convex hull for the points
@@ -86,13 +82,11 @@ select
 	st_convexhull(st_collect(qry.geom))
 from (
 select 
-			
-		--	st_distance(vb.geom_5973, st_centroid(j.geom)) as dist,
-			vb.geom_5973 as geom
-			from nibio.jordbruk j, nibio.v_bygning_bolig vb
+		--	st_distance(vb.geom, st_centroid(f.geom)) as dist,
+			vb.geom as geom
+			from field f, v_bygning_bolig vb
 
-order by vb.geom_5973 <->  st_centroid(j.geom)
+order by vb.geom <->  st_centroid(f.geom)
+-- count of amount of households
 limit 6623
 ) qry;
-
-
